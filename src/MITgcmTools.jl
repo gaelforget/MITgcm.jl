ac09c5b59a6f13d85af89081a24dfd44b0853262
gaelforget <gforget@mit.edu>
2020-01-25 04:28:58 -0500

    functions mv'ed from MeshArrays

--- src/MITgcmTools.jl
+++ src/MITgcmTools.jl
@@ -1,5 +1,11 @@
 module MITgcmTools
 
-greet() = print("Hello World!")
+using Plots, Dates, NetCDF, Printf, MeshArrays
+
+include("BasicPlots.jl")
+include("FormatConversions.jl")
+include("ReadNCTiles.jl")
+
+export qwckplot, convert2array, convert2gcmfaces, read_bin, read_nctiles
 
 end # module
