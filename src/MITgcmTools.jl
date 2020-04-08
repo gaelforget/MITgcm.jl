dc2bb9915147d9697e0133da2a2a0946a3fbf8bb
gaelforget <gforget@mit.edu>
2020-04-07 22:38:13 -0400

    port jmd94 from earlier language implementation

--- src/MITgcmTools.jl
+++ src/MITgcmTools.jl
@@ -5,10 +5,12 @@ using Plots, Dates, NetCDF, Printf, MeshArrays, SparseArrays, MAT, JLD
 include("BasicPlots.jl")
 include("FormatConversions.jl")
 include("ReadFiles.jl")
+include("PhysicalOceanography.jl")
 
 export qwckplot, convert2array, convert2gcmfaces, read_bin
 export read_nctiles, findtiles, MetaFileRead
 export prep_MTRX, MatrixInterp, read_SPM
 export parsemeta, readAvailDiagnosticsLog
+export jmd94
 
 end # module
