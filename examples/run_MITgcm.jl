16fe51ecc071c600efd2a8e94fd2294515a477d4
gaelforget <gforget@mit.edu>
2021-03-14 15:44:21 -0400

    select, run, etc verif exps (Pluto notebook)

---
+++ examples/run_MITgcm.jl
@@ -0,0 +1,39 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
+# ╔═╡ 8cf4d8ca-84eb-11eb-22d2-255ce7237090
+begin
+	using MITgcmTools, PlutoUI, Printf
+	exps=verification_experiments()
+	🏁 = "🏁"
+end
+
+# ╔═╡ a28f7354-84eb-11eb-1830-1f401bf2db97
+@bind myexp Select([exps[i].name for i in 1:length(exps)])
+
+# ╔═╡ f91c3396-84ef-11eb-2665-cfa350d38737
+begin
+	iexp=findall([exps[i].name==myexp for i in 1:length(exps)])[1]
+	TextField((30, 10), "name = $(exps[iexp].name)\n\nbuild  = $(exps[iexp].build) \n\nrun    = $(exps[iexp].run)")
+end
+
+# ╔═╡ f588eaba-84ef-11eb-0755-bf1b85b2b561
+("compile","run","monitor","plot")
+
+# ╔═╡ Cell order:
+# ╟─8cf4d8ca-84eb-11eb-22d2-255ce7237090
+# ╟─f91c3396-84ef-11eb-2665-cfa350d38737
+# ╟─a28f7354-84eb-11eb-1830-1f401bf2db97
+# ╠═f588eaba-84ef-11eb-0755-bf1b85b2b561
