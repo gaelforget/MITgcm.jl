f9ed60216620c23d50dfd3f83ea5325fafab7961
gaelforget <gforget@mit.edu>
2021-03-15 13:29:45 -0400

    add try/catch and markdown headers + images

--- examples/monitor_run.jl
+++ examples/monitor_run.jl
@@ -21,16 +21,46 @@ begin
 end
 
 # ╔═╡ f588eaba-84ef-11eb-0755-bf1b85b2b561
-("inspect","monitor","plot")
+begin
+	imgA="https://user-images.githubusercontent.com/20276764/111042787-12377e00-840d-11eb-8ddb-64cc1cfd57fd.png"
+	imgB="https://user-images.githubusercontent.com/20276764/97648227-970b9780-1a2a-11eb-81c4-65ec2c87efc6.png"
+	md"""# monitor_run.jl
+
+	### 
+
+
+	Here we use scan an MITgcm run folder interactivetly to generate something like this:
+	
+	$(Resource(imgA, :width => 240))
+	
+	### 
+	
+	$(Resource(imgB, :width => 120))
+	"""
+end
+
+# ╔═╡ 98b6621c-85ab-11eb-29d1-af0433598c6a
+	md"""## Select model configuration:
+	
+	_Note: this will update the multiple-choices menu sequence below_
+	"""
 
 # ╔═╡ a28f7354-84eb-11eb-1830-1f401bf2db97
-@bind myexp Select([exps[i].name for i in 1:length(exps)],default="advect_cs")
+@bind myexp Select([exps[i].name for i in 1:length(exps)],default="advect_xy")
 
 # ╔═╡ f91c3396-84ef-11eb-2665-cfa350d38737
 begin
 	iexp=findall([exps[i].name==myexp for i in 1:length(exps)])[1]
 	TextField((100, 8), "name = $(exps[iexp].name)\n\nbuild  = $(exps[iexp].build) \n\nrun    = $(exps[iexp].run)")
 end
+
+# ╔═╡ f051e094-85ab-11eb-22d4-5bd61ac572a1
+md"""## Select a namelist and parameter group
+
+_Note: `data` and `PARM01`, e.g., should be found in any model run directory,_ **once the model has been run for that configuration**
+
+_Note: one can use e.g. `run MITgcm.jl` notebook or the `MITgcm run()` function to rerun the various model configurations_
+"""
 
 # ╔═╡ d7f2c656-8512-11eb-2fdf-47a3e57a55e6
 begin
@@ -45,7 +75,11 @@ begin
 		tmpA=tmpA[findall([tmpA[i][1:4]=="data" for i in 1:length(tmpA)])]
 	end
 	dats=list_namelist_files(pth)
-	@bind mydats Select([dats[i] for i in 1:length(dats)])
+	try
+		@bind mydats Select([dats[i] for i in 1:length(dats)])
+	catch e
+		"Error: could not find any namelist in $(pth)"
+	end
 end
 
 # ╔═╡ 348c692e-84fe-11eb-3288-dd0a1dedce90
@@ -56,7 +90,11 @@ begin
 end
 
 # ╔═╡ ca7bb004-8510-11eb-379f-632c3b40723d
-@bind mynamelist Select([String(keys(namelist)[i]) for i in 1:length(namelist)])
+try
+	@bind mynamelist Select([String(keys(namelist)[i]) for i in 1:length(namelist)])
+catch e
+	"Error: could not find any namelist in $(pth)"
+end
 
 # ╔═╡ 9bdb94da-8510-11eb-01a6-c9a1519baa68
 begin
@@ -71,12 +109,18 @@ begin
 end
 
 # ╔═╡ a3392068-8514-11eb-14ab-4b807c5325d3
-TextField((40, length(params)+2),params_txt)
+try
+	TextField((40, length(params)+2),params_txt)
+catch e
+	"Error: could not find any namelist in $(pth)"
+end	
 
 # ╔═╡ Cell order:
 # ╟─f588eaba-84ef-11eb-0755-bf1b85b2b561
+# ╟─98b6621c-85ab-11eb-29d1-af0433598c6a
 # ╟─a28f7354-84eb-11eb-1830-1f401bf2db97
 # ╟─f91c3396-84ef-11eb-2665-cfa350d38737
+# ╟─f051e094-85ab-11eb-22d4-5bd61ac572a1
 # ╟─d7f2c656-8512-11eb-2fdf-47a3e57a55e6
 # ╟─ca7bb004-8510-11eb-379f-632c3b40723d
 # ╟─a3392068-8514-11eb-14ab-4b807c5325d3
