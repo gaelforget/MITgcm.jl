ce2a008a7e8382c160a7e4e25358976a3dca4304
gaelforget <gforget@mit.edu>
2021-04-04 22:46:47 -0400

    update and improve HS94 notebooks

--- examples/HS94_particles.jl
+++ examples/HS94_particles.jl
@@ -4,25 +4,62 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ e032b6d4-959d-11eb-28df-2112db1b1e4e
+# ╔═╡ 02005854-3442-4c65-910b-e8a000805d17
 begin
+	md"""# HS94_particles.jl
+
+	### 
+
+
+	Here we setup, run and plot MITgcm output via **`IndividualDisplacements.jl`** to generate something like this:
 	
-	## Lagrangian particle advection
-	
-	using MITgcmTools, MeshArrays, IndividualDisplacements, OrdinaryDiffEq
-	
-	#Model grid, which should be made provided in folder `pth` below
+	![plot](https://user-images.githubusercontent.com/20276764/111042787-12377e00-840d-11eb-8ddb-64cc1cfd57fd.png)
+	"""
+end
+
+# ╔═╡ a6ebb38e-2dcd-4126-bfa1-220df3180b94
+begin
+	#Path to model output
 	pth="run_HS94/"
 	
+	md""" ### Julia Packages And Model Files
+	
+	Model grid and output files are expected to be found in _$(pth)_.
+	First, a list of grid variables should appear just below if files are indeed found as expected.
+	"""
+end
+
+# ╔═╡ bf462d7b-28af-4fc5-9952-e631051df4cd
+begin
+	#packages for Lagrangian particle advection etc	
+	using MITgcmTools, MeshArrays, IndividualDisplacements, OrdinaryDiffEq
+	p=dirname(pathof(IndividualDisplacements))
+	include(joinpath(p,"../examples/recipes_plots.jl"))
+
+	#Read and write functions for cube-sphere grid as configured in this model run
 	readcube(xx::Array,x::MeshArray) = read(cube2compact(xx),x)
 	readcube(fil::String,x::MeshArray) = read(fil::String,x::MeshArray)
 	writecube(x::MeshArray) = compact2cube(write(x))
 	writecube(fil::String,x::MeshArray) = write(fil::String,x::MeshArray)
 	
+	#Load grid files to memory
 	γ=gcmgrid(pth,"CubeSphere",6,fill((32, 32),6), [192 32], Float64, readcube, writecube)
 	Γ = GridLoad(γ)
-	
-	#parameters: level, time, U/V files, grid factors
+	keys(Γ)
+end
+
+# ╔═╡ 05cbf6ff-032f-4090-8981-da931f9e1521
+md""" ### Functions And Data Structures
+
+Individuals 𝐼 are those that will be advected by the flow fields using the `∫!` function. 
+This can be done in a time loop with `update_FlowFields!` used to load flow fields in a 
+sequence as shown below.
+"""		
+
+# ╔═╡ e032b6d4-959d-11eb-28df-2112db1b1e4e
+begin
+		
+	#Parameters: level, time, U/V files, grid factors
 	k=3
 	dt=7200.0
 	t00=43200.0*600
@@ -30,6 +67,7 @@ begin
 	tmp=readdir(pth)
 	Ul=tmp[findall(occursin.("U.0000",tmp).*occursin.(".data",tmp))]
 	Vl=tmp[findall(occursin.("V.0000",tmp).*occursin.(".data",tmp))]
+	ndt=length(Ul)-1
 	
 	𝐷=(pth=pth,k=k,dt=dt,t00=t00,Γ=Γ,
 	    U=Ul,V=Vl,tmp=MeshArray(γ,Float64,5),    
@@ -75,22 +113,33 @@ begin
 	    𝑃.𝑇[:]=[t0,t1]
 	end
 	
+	𝐼
+end
+
+# ╔═╡ 5a7af9d3-b800-44fd-9ffe-0580b51ee70a
+md""" ### Trajectory Computation
+
+The particle trajectory output is in tabular format. To save results to file, a simple yet general method is :
+
+```
+using Dates, JLD2
+M=(timestamp=now(),author="me",run="HS94")
+I=(metadata=M, ID=𝐼.🆔, record=𝐼.🔴, position=𝐼.📌)
+@save "HS94.jld2" I
+```
+"""
+
+# ╔═╡ 7cc13ee0-ad61-4274-9c94-cbfa3974046b
+begin
 	#Read velocity fields and compute trajectories
 	update_FlowFields!(𝐼,𝐷)
 	∫!(𝐼)
 	
 	#Plot initial and final locations
-	p=dirname(pathof(IndividualDisplacements))
-	include(joinpath(p,"../examples/recipes_plots.jl"))
 	f1=plot(𝐼)
-	
-	#To save this plot, e.g. : 
-	#```
-	#savefig(tempdir()*"/"*"hs94.cs.particles.png")
-	#```
-	
+		
 	#Carry on for several time intervals (𝐼.𝑃.𝑇 .+ 𝐷.dt and so on)
-	for tt=1:length(𝐷.U)-2
+	for tt=1:ndt-1
 	    update_FlowFields!(𝐼,𝐷)
 	    ∫!(𝐼)
 	end
@@ -98,16 +147,23 @@ begin
 	
 	#Add longitude and latitude
 	add_lonlat!(𝐼.🔴,𝐷.XC,𝐷.YC)
-	
-	#To save results, e.g. :
-	#```
-	#using Dates, JLD2
-	#M=(timestamp=now(),author="me",run="HS94")
-	#I=(metadata=M, ID=𝐼.🆔, record=𝐼.🔴, position=𝐼.📌)
-	#@save "HS94.jld2" I
-	#```
-	
 end
+
+# ╔═╡ 5129ac14-0861-40b5-b97e-0d0edd37849f
+md""" ### Plots
+
+The plots below show:
+
+- 1. initial positions (red) and after 1 day (blue; `x,y`)
+- 2. initial and final positions ($ndt days; `x,y`)
+- 3. initial and final coordinates ($ndt days; `lon,lat`)
+
+To save a plot, add: 
+
+```
+savefig(tempdir()*"/"*"hs94.cs.particles.png")
+```
+"""
 
 # ╔═╡ a34adf36-81de-42c2-8407-d408041193b6
 f1
@@ -115,7 +171,18 @@ f1
 # ╔═╡ a1fdaf62-1b80-481e-a5c7-45e82c540368
 f2
 
+# ╔═╡ c473fb90-8901-466e-a8e3-c7efd189e1ba
+	f3=plot(𝐼)
+
 # ╔═╡ Cell order:
+# ╠═02005854-3442-4c65-910b-e8a000805d17
+# ╟─a6ebb38e-2dcd-4126-bfa1-220df3180b94
+# ╟─bf462d7b-28af-4fc5-9952-e631051df4cd
+# ╟─05cbf6ff-032f-4090-8981-da931f9e1521
 # ╟─e032b6d4-959d-11eb-28df-2112db1b1e4e
+# ╟─5a7af9d3-b800-44fd-9ffe-0580b51ee70a
+# ╟─7cc13ee0-ad61-4274-9c94-cbfa3974046b
+# ╟─5129ac14-0861-40b5-b97e-0d0edd37849f
 # ╟─a34adf36-81de-42c2-8407-d408041193b6
 # ╟─a1fdaf62-1b80-481e-a5c7-45e82c540368
+# ╟─c473fb90-8901-466e-a8e3-c7efd189e1ba
--- examples/HS94_plotmap.jl
+++ examples/HS94_plotmap.jl
@@ -7,32 +7,53 @@ using InteractiveUtils
 # ╔═╡ 3668f786-9597-11eb-01a1-87d34b49eef9
 begin
 	
+	#packages for I/O, interpolation, etc	
 	using MITgcmTools, MeshArrays, Plots
 	
+	#Path to model output
 	pth="run_HS94/"
+
 	
+	#Read and write functions for cube-sphere grid as configured in this model run	
 	readcube(xx::Array,x::MeshArray) = read(cube2compact(xx),x)
 	readcube(fil::String,x::MeshArray) = read(fil::String,x::MeshArray)
 	writecube(x::MeshArray) = compact2cube(write(x))
 	writecube(fil::String,x::MeshArray) = write(fil::String,x::MeshArray)
 	
+	#Load grid files to memory
 	γ=gcmgrid(pth,"CubeSphere",6,fill((32, 32),6), [192 32], Float64, readcube, writecube)
 	Γ = GridLoad(γ)
 	
 	## Interpolation setup for plotting
-	
 	lon=[i for i=-179.5:1.0:179.5, j=-89.5:1.0:89.5]
 	lat=[j for i=-179.5:1.0:179.5, j=-89.5:1.0:89.5]
 	(f,i,j,w,_,_,_)=InterpolationFactors(Γ,vec(lon),vec(lat))
 	IntFac=(f,i,j,w)
 	
-	##
+	🏁 = "🏁"
+end
+
+# ╔═╡ 19095067-33f5-495f-bc4d-ee6dacbf6ca8
+begin
+	md"""# HS94_plotmap.jl
+
+	### 
+
+
+	Here we read MITgcm output for temperature and interpolate it via **`MeshArrays.jl`** to generate something like this:
 	
+	![plot](https://user-images.githubusercontent.com/20276764/113531401-b1780d00-9596-11eb-8e96-990cf9533ada.png)
+	"""
+end
+
+# ╔═╡ 56a76f42-7d83-4600-a9a2-2b675b6efcaa
+begin
+	#list of output files (1 per time record)
 	ff=readdir(pth); fil="T.0000"
-	ff=ff[findall(occursin.(fil,ff).*occursin.(".data",ff))]
-	
+	ff=ff[findall(occursin.(fil,ff).*occursin.(".data",ff))]	
 	nt=length(ff)
 	
+	#function used to plot one time record
 	function myplot(fil)
 	    T=read(pth*fil,MeshArray(γ,Float64))
 	    TT=Interpolate(T,IntFac...)
@@ -41,24 +62,22 @@ begin
 	
 	##
 	
-	#dt=Int(ceil(nt/100))
+	f1=myplot(ff[end])	
+	🏁
+end
+
+# ╔═╡ ee0e6f28-aa26-48de-8ddd-8bb2d1102ee9
+begin
 	dt=6
-	
-	#g1 = @gif for i ∈ 1:dt:nt
-	#    myplot(ff[i])
-	#end
-	
-	myplot(ff[end])
-	
-	#```
-	#anim = @animate for i ∈ 1:dt:nt
-	#    myplot(ff[i])
-	#end
-	#pp=tempdir()*"/"
-	#gif(anim,pp*"hs94.cs.gif", fps = 8)
-	#```
-	
+	anim = @animate for i ∈ 1:dt:nt
+	    myplot(ff[i])
+	end
+	pp=tempdir()*"/"
+	gif(anim,pp*"hs94.cs.gif", fps = 8)
 end
 
 # ╔═╡ Cell order:
+# ╟─19095067-33f5-495f-bc4d-ee6dacbf6ca8
 # ╟─3668f786-9597-11eb-01a1-87d34b49eef9
+# ╟─56a76f42-7d83-4600-a9a2-2b675b6efcaa
+# ╟─ee0e6f28-aa26-48de-8ddd-8bb2d1102ee9
