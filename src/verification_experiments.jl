df2f540130cbbef1ef2d2dda02658195097f07c6
gaelforget <gforget@mit.edu>
2021-07-09 07:24:06 +0200

    mv verification_experiments to separate file

---
+++ src/verification_experiments.jl
@@ -0,0 +1,45 @@
+
+"""
+verification_experiments()
+
+Get list of all `most-standard` configurations of `MITgcm` and return as an Array of `MITgcm_config`
+
+```
+using MITgcmTools
+exps=verification_experiments()
+```
+"""
+function verification_experiments()
+pth=joinpath(MITgcm_path[1],"verification")
+lst=readdir(pth)
+tmp=[isfile(joinpath(pth,i,"code","packages.conf")) for i in lst]
+tmp2=[isfile(joinpath(pth,i,"code","SIZE.h")) for i in lst]
+lst=lst[findall(tmp.|tmp2)]
+
+pkg_build=fill(String[],size(lst))
+pkg_run=fill(String[],size(lst))
+for i in 1:length(lst)
+    fil=joinpath(pth,lst[i],"code","packages.conf")
+    if isfile(fil)
+        tmp1=read(fil,String)
+        tmp1=split(tmp1,"\n")
+        tmp1=tmp1[findall((!isempty).(tmp1))]
+        pkg_build[i]=tmp1[findall(first.(tmp1).!=='#')]
+    end
+
+    fil=joinpath(pth,lst[i],"input","data.pkg")
+    tmp1=read(fil,String)
+    tmp1=split(tmp1,"\n")
+    tmp1=tmp1[findall((!isempty).(tmp1))]
+    tmp1=tmp1[findall(first.(tmp1).!=='#')]
+    pkg_run[i]=tmp1[findall([!occursin("&",i) for i in tmp1])]
+end
+
+exps=fill(MITgcm_config(),length(lst))
+for i in 1:length(lst)
+    ID = UUIDs.uuid4()
+    exps[i]=MITgcm_config(configuration=lst[i],ID=ID)
+end
+
+return exps
+end
