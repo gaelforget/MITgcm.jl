0d2bfd4edda875386167ea34ac0852a60c5cf34f
gaelforget <gforget@mit.edu>
2021-08-26 18:22:02 -0400

    merge mnc and mdsio loops

--- examples/MITgcm_scan_output.jl
+++ examples/MITgcm_scan_output.jl
@@ -41,6 +41,7 @@ begin
 		sc[i]=MITgcmTools.scan_rundir(rundir)
 	end
 	
+	list_exps=collect(1:length(exps))
 	[exps[i].configuration for i in 1:length(exps)]
 end
 
@@ -54,19 +55,23 @@ list_mnc=findall([sc[i].params_files.use_mnc for i in 1:length(exps)])
 (length(list_mdsio),length(list_mnc),length(exps))
 
 # ╔═╡ adf96bf1-b405-4405-a747-e2835b670a25
-md"""## 1. Select Config. : binary output cases
+md"""## 1. Select Configuration
 
 _Note: this will update the plot and text display below_
 """
 
 # ╔═╡ 7c37d2bd-2a10-4a42-9029-87d636c3a054
-@bind ii NumberField(1:length(list_mdsio); default=2)
+@bind ii NumberField(1:length(list_exps); default=2)
 
 # ╔═╡ 211ab33e-d482-49dd-9448-0f5c6e63a280
 begin
-	i=list_mdsio[ii]
-	Γ=MITgcmTools.GridLoad_mdsio(exps[i])
-	
+	i=list_exps[ii]
+	if sc[i].params_files.use_mdsio
+		Γ=MITgcmTools.GridLoad_mdsio(exps[i])
+	else
+		Γ=MITgcmTools.GridLoad_mnc(exps[i])
+	end
+
 	is2d=sum(sc[i].params_files.ioSize.>1)==2
 	if sc[i].params_grid.usingCartesianGrid
 		 f=plot_as_plane(Γ)
@@ -79,7 +84,7 @@ begin
 end
 
 # ╔═╡ a444cf7e-cbe1-4e13-981b-184f1a64d3d5
- exps[list_mdsio[ii]]
+ exps[list_exps[ii]]
 
 # ╔═╡ 49e5553c-b316-4c2c-821a-0dd6148006dc
 sc[i].params_grid
@@ -89,39 +94,6 @@ begin
 	dd=Float64.(Γ.Depth)
 	minimum(dd),maximum(dd)
 end
-
-# ╔═╡ d43a7659-9466-449c-9bf1-0c7ee668ce82
-md"""## 2. Select Config. : netcdf output cases
-
-_Note: this will update the plot and text display below_
-"""
-
-# ╔═╡ 9a6583c8-325d-49e9-95cb-f10a33d16394
-@bind jj NumberField(1:length(list_mnc); default=2)
-
-# ╔═╡ ce15c4e2-e6d9-4908-a336-14de39fd3c20
-begin
-	j=list_mnc[jj]
-	rundir=joinpath(rep,exps[j].configuration,"run"); exps[j]
-	
-	Γ_mnc=MITgcmTools.GridLoad_mnc(exps[j])
-
-	is2d_mnc=sum(sc[j].params_files.ioSize.>1)==2
-	if sc[j].params_grid.usingCartesianGrid
-		f_mnc=plot_as_plane(Γ_mnc)
-	elseif is2d_mnc
-		f_mnc=plot_as_sphere(Γ_mnc)
-	else
-		f_mnc="no display available"
-	end
-	f_mnc
-end
-
-# ╔═╡ 52500893-60c9-4f62-ae38-9e07ee76a34b
- exps[list_mnc[jj]]
-
-# ╔═╡ 4f9c9197-9d83-4545-8d41-92c904a29c9f
-sc[list_mnc[jj]].params_grid
 
 # ╔═╡ Cell order:
 # ╟─f883622e-dada-4acf-9c90-2c3a3373da66
@@ -136,8 +108,3 @@ sc[list_mnc[jj]].params_grid
 # ╟─a444cf7e-cbe1-4e13-981b-184f1a64d3d5
 # ╟─49e5553c-b316-4c2c-821a-0dd6148006dc
 # ╟─92b6319a-a56d-483e-a7eb-6f71966364c5
-# ╟─d43a7659-9466-449c-9bf1-0c7ee668ce82
-# ╟─9a6583c8-325d-49e9-95cb-f10a33d16394
-# ╟─ce15c4e2-e6d9-4908-a336-14de39fd3c20
-# ╟─52500893-60c9-4f62-ae38-9e07ee76a34b
-# ╟─4f9c9197-9d83-4545-8d41-92c904a29c9f
