5aaff57e0947533db2f5e28444290045c071a8c7
gaelforget <gforget@mit.edu>
2021-08-26 20:46:26 -0400

    export newly added functions

--- src/MITgcmTools.jl
+++ src/MITgcmTools.jl
@@ -15,8 +15,8 @@ export MITgcm_config, MITgcm_namelist, MITgcm_launch
 export testreport, build, compile, setup, clean
 #export pause, stop, clock, monitor, train, help
 export verification_experiments, read_namelist, write_namelist
-export read_mdsio, read_meta, read_available_diagnostics
-export read_bin, read_flt, read_nctiles, findtiles, parse_param
+export read_mdsio, read_meta, read_available_diagnostics, scan_rundir
+export read_bin, read_flt, read_mnc, read_nctiles, findtiles, parse_param
 export cube2compact, compact2cube, convert2array, convert2gcmfaces
 export SeaWaterDensity, MixedLayerDepth
 
