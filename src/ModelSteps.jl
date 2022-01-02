012fddc88e9a45607aa05c92007168fd63eacf8c
Gael Forget <gforget@mit.edu>
2022-01-01 22:02:01 -0500

    Update ModelSteps.jl

--- src/ModelSteps.jl
+++ src/ModelSteps.jl
@@ -135,8 +135,8 @@ end
 
 Create a `run/` folder and link everything there as needed to be ready to run model as 
 normally done for most-standard MITgcm configurations (incl. `prepare_run` and `mitgcmuv`).
-Call `git_log_init(config)` to setup git tracker and `put!(config.channel,MITgcm_launch)` 
-to be executed via `launch(config)` later.
+Call `ClimateModels.git_log_init(config)` to setup git tracker and 
+`put!(config.channel,MITgcm_launch)` to be executed via `launch(config)` later.
 
 (part of the climate model interface as specialized for `MITgcm`)
 """
@@ -202,7 +202,7 @@ function setup(config::MITgcm_config)
     end
 
     logdir=joinpath(config.folder,string(config.ID),"log")
-    !isdir(logdir) ? git_log_init(config) : nothing
+    !isdir(logdir) ? ClimateModels.git_log_init(config) : nothing
 
     #Replace namelists with editeable versions in log/
     #
