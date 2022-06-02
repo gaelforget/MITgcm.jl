740235bcff71d33923e6274f27e521a24af0b0d6
Barbie <birdy@clytemnestra.local>
2022-06-01 21:08:05 -0400

    cleaned up darwin-run

--- examples/darwin-tutorial/darwin-run.jl
+++ examples/darwin-tutorial/darwin-run.jl
@@ -60,71 +60,11 @@ rundir = joinpath(folder, config_id, "run")
 # timing 
 update_param("data", "PARM03", "nenditer", 2880) # end after 1 year
 
+# temperature
 update_param("data", "PARM01", "tRef", 30.0)
+
+# Prochlorococcus
 update_param("data.ptracers", "PTRACERS_PARM01", "PTRACERS_ref( :,21)", 1e-3)
-
-# # enter the name of the data file you want to modify 
-# data_file = "data" # CHANGE ME
-
-# # read the contents of the data file into a namelist 
-# fil = joinpath(rundir, data_file)
-# nml = read(fil, MITgcm_namelist())
-# println("Reading from file ", fil)
-# println("groups: ", nml.groups)
-
-# # which param group do you want to modify?
-# nmlgroup = "PARM01" # CHANGE ME
-# group_idx =findall(nml.groups.==Symbol(nmlgroup))[1]
-# parms = nml.params[group_idx]
-# println("parms: ", parms)
-
-# # what parameter do you want to modify?
-# p_name = "tRef" # CHANGE ME
-# p_value = "30.0" # CHANGE ME
-
-# # write changed parameter
-# tmptype=typeof(nml.params[group_idx][Symbol(p_name)])
-# nml.params[group_idx][Symbol(p_name)]=parse(tmptype,p_value)
-# tmpfil=joinpath(rundir,data_file)
-# rm(tmpfil)
-# write(tmpfil,nml)
-# tmpfil=joinpath("tracked_parameters",data_file)
-# ClimateModels.git_log_fil(config_obj,tmpfil,"updated $(p_name) parameter file in $(data_file) to $(p_value)")
-
-
-# ##################
-# # add pro
-# ##################
-# # ### goal: turn on pro
-# # enter the name of the data file you want to modify 
-# data_file = "data.ptracers" # CHANGE ME
-
-# # read the contents of the data file into a namelist 
-# fil = joinpath(rundir, data_file)
-# nml = read(fil, MITgcm_namelist())
-# println("Reading from file ", fil)
-# println("groups: ", nml.groups)
-
-# # which param group do you want to modify?
-# nmlgroup = "PTRACERS_PARM01" # CHANGE ME
-# group_idx =findall(nml.groups.==Symbol(nmlgroup))[1]
-# parms = nml.params[group_idx]
-# println("parms: ", parms)
-
-# # what parameter do you want to modify?
-# p_name = "PTRACERS_ref( :,21)" # CHANGE ME
-# p_value = "1E-3" # CHANGE ME
-
-# # write (or add) changed parameter
-# #tmptype=typeof(nml.params[group_idx][Symbol(p_name)])
-# println(tmptype)
-# tmptype=typeof(0.5)
-# nml.params[group_idx][Symbol(p_name)]=parse(tmptype,p_value)
-# tmpfil=joinpath(rundir,data_file)
-# rm(tmpfil)
-# write(tmpfil,nml)
-# tmpfil=joinpath("tracked_parameters",data_file)
-# ClimateModels.git_log_fil(config_obj,tmpfil,"updated $(p_name) parameter file in $(data_file) to $(p_value)")
 
 
 ##################
