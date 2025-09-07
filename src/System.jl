fe9dcabb41216698496448c35a259a45b53c020b
gaelforget <gforget@mit.edu>
2025-09-07 08:04:27 -0400

    add mpi in system_check. output as MITgcm_system_check

--- src/System.jl
+++ src/System.jl
@@ -1,13 +1,23 @@
 
 using StyledStrings
 
-test_run(;configuration="advect_xy",exe="")=begin
+test_run(;configuration="advect_xy",exe="",mpi=false)=begin
   MC=MITgcm_config(configuration=configuration)
   setup(MC)
-  if isempty(exe)
+  if isempty(exe)&&mpi
+    println("compiling and running test with MPI")
+    MC.inputs[:setup][:build][:options]=MC.inputs[:setup][:build][:options]*" -devel -ds -ieee -mpi"
+    SIZE_in=joinpath(pathof(MC),"MITgcm","verification","advect_xy","code","SIZE.h_MPI")
+    SIZE_out=joinpath(pathof(MC),"MITgcm","verification","advect_xy","build","SIZE.h")
+    cp(SIZE_in,SIZE_out)
+    push!(MC.inputs[:setup][:main],(:command => "mpirun -np 2 ./mitgcmuv"))
+    build(MC)
+  elseif isempty(exe)
+    println("compiling and running test")
     MC.inputs[:setup][:build][:options]=MC.inputs[:setup][:build][:options]*" -devel -ds -ieee"
     build(MC)
   else
+    println("running test with precompiled model")
     pth=joinpath(MC,"run","mitgcmuv")
     rm(pth)
     symlink(exe,pth)
@@ -22,7 +32,7 @@ end
 
 Run a suite of diagnostics, including a test run of MITgcm.
 """
-system_check(;setenv=false,exe="")=begin
+system_check(;setenv=false,exe="",mpi=false)=begin
 
   setenv ? set_environment_variables_to_default() : nothing
 
@@ -41,13 +51,17 @@ system_check(;setenv=false,exe="")=begin
   end
   push!(tests,("MITgcm download"=>tst[1]))
     
-  MC=test_run(configuration="advect_xy",exe=exe)
+  config="advect_xy"
+  MC=test_run(configuration=config,exe=exe,mpi=mpi)
 
   genmake_log=joinpath(pathof(MC),"MITgcm","verification","advect_xy","build","genmake.log")
   if isfile(genmake_log)
     genmake_log,genmake_state=scan_genmake_log(MC)
     push!(tests,("genmake_log"=>genmake_log))
     push!(tests,("genmake_state"=>genmake_state))
+  else
+    push!(tests,("genmake_log"=>ClimateModels.OrderedDict()))
+    push!(tests,("genmake_state"=>ClimateModels.OrderedDict()))
   end
 
   RS=scan_rundir(joinpath(MC,"run"))
@@ -83,7 +97,18 @@ system_check(;setenv=false,exe="")=begin
     end
   end
 
-  tests
+  MITgcm.MITgcm_system_check(
+    download=tests["MITgcm download"],
+    complete=tests["run complete"],
+    name=config,
+    folder=tests["test folder"],
+    NETCDF_ROOT=tests["NETCDF_ROOT"],
+    MPI_INC_DIR=tests["MPI_INC_DIR"],
+    mpi=mpi,
+    genmake_log=tests["genmake_log"],
+    genmake_state=tests["genmake_state"],
+  )
+
 end
 
 """
